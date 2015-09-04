72536ad7e3a57964d0bc34eb551e0ab8dd8dc3ef
Vladislav Baimurzin <baimurzin.719@gmail.com>
2015-09-04 23:34:16 +0300

    added some features

--- core/bot.js
+++ core/bot.js
@@ -1,38 +1,100 @@
 var EventEmitter = require("events").EventEmitter,
     fs = require("fs"),
     util = require("util"),
-    Steam = require("steam");
+    crypto = require('crypto'),
+    Steam = require("steam"),
+    SteamTradeOffers = require('steam-tradeoffers'),
+    SteamWebLogOn = require('steam-weblogon');
 
     var steamClient = {},
-        steamUser = {};
+        steamUser = {},
+        offers, steamWebLogOn, botSessionId, botCookie;
 
 var Bot = function (commonSteamClient, debug) {
     EventEmitter.call(this);
+    offers = new SteamTradeOffers();
     steamClient = commonSteamClient;
     this._isRunning = false;
     this.debug = debug || false;
-    util.log("instance created ");
 };
 util.inherits(Bot, EventEmitter);
 
 Bot.prototype.start = function (config) {
+    var self = this;
     if (steamClient.connected){
-        this.emit('log', 'log!!!');
-        util.log("con!!")
+
+        self._config = config;
         steamUser = new Steam.SteamUser(steamClient);
-        steamUser.logOn({
-            account_name: config.username,
-            password: config.password,
-            auth_code: 'R6GCM'
+        steamWebLogOn = new SteamWebLogOn(steamClient, steamUser);
+        var logOnOptions = {};
+        logOnOptions.account_name = config.username;
+        logOnOptions.password = config.password;
+
+        //sentry or auth_code
+        if (config.sha_sentryfile) {
+            logOnOptions.sha_sentryfile = getSHA(config.sha_sentryfile);
+            self.emit('sentry found');
+        } else if (config.auth_code) {
+            self.emit('no sentry using auth_code')
+
+            logOnOptions.auth_code = config.auth_code;
+        }
+
+        steamUser.logOn(logOnOptions);
+        steamUser.on('updateMachineAuth', function (sentry, callback) {
+            fs.writeFileSync('sentry_'+self._config.username, sentry.bytes);
+            self.emit('sentry file created');
+
+            callback({
+                sha_file: getSHA(sentry.bytes)
+            });
+        });
+        steamClient.on('logOnResponse', function (logonResp) {
+            if (logonResp.eresult == Steam.EResult.OK) {
+                self._isRunning = true;
+                self.emit('debug','bot started: ' + self._config.username);
+                steamWebLogOn.webLogOn(function (webSessionId, cookies) {
+                    botSessionId = webSessionId;
+                    botCookie = cookies;
+                    offers.setup({
+                        sessionId: botSessionId,
+                        webCookie: botCookie
+                    });
+                    self.emit('debug', 'cookie configured with session ID: ' + botSessionId + " on bot : " + self._config.username);
+                    self.emit('ready');
+                });
+            }
 
         });
-        steamClient.on('logOnResponse', function () {
-            console.log('bot auth');
-        })
     }
     else {
-        this.emit('error', 'check connection ot steamClient');
+        this.emit('debug', 'check connection ot steamClient');
     }
 };
+
+Bot.prototype.stop = function () {
+    var self = this;
+    self._isRunning = false;
+};
+
+Bot.prototype.getBotInventory = function (appId, contextId) {
+    var self = this;
+    self.emit('debug', botSessionId + "TEST");
+    offers.loadMyInventory({
+        appId: appId,
+        contextId: contextId
+    }, function (err, items) {
+        if (err) {
+            self.emit('errors', 'Cannot load inventory');
+        }
+        var item = [], num = 0;
+        self.emit('debug', fs.writeFileSync("items",JSON.stringify(items)));
+        return items;
+    });
+};
+
+function getSHA(bytes) {
+    return crypto.createHash('sha1').update(bytes).digest();
+}
 
 module.exports = Bot;
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var Bot = require("./core/bot");
-var Steam = require("steam");
+var Steam = require("steam"),
+    fs = require('fs');
 
 var steam = new Steam.SteamClient();
 var user = new Steam.SteamUser(steam);
@@ -7,14 +8,18 @@ var user = new Steam.SteamUser(steam);
 
 steam.connect();
 steam.on('connected', function () {
-    var bot = new Bot(steam);
+    var bot = new Bot(steam, true);
     console.log('connected')
-    bot.start({
-        username: '',
-        password: ''
+    var logOnOptions = {
+        username: 'baimurzin719',
+        password: '5BapsxsM',
+        //auth_code: 'WPTR2'
+    };
+    logOnOptions.sha_sentryfile = fs.readFileSync('sentry_'+logOnOptions.username);
+    bot.start(logOnOptions);
+    bot.on('ready', function () {
+        bot.getBotInventory(570, 2);
+
     });
-
-    bot.on('log', console.log);
+    bot.on('debug', console.log);
 });
-
-
